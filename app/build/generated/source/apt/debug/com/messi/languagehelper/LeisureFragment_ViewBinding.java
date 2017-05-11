9e0e221cf4ea8c356d542c06ca703d33d4370d75
luli <meixililulu@163.com>
2017-05-11 22:47:14 +0800

    v3.8 pre tablayout

--- app/build/generated/source/apt/debug/com/messi/languagehelper/LeisureFragment_ViewBinding.java
+++ app/build/generated/source/apt/debug/com/messi/languagehelper/LeisureFragment_ViewBinding.java
@@ -15,7 +15,7 @@ import java.lang.Override;
 public class LeisureFragment_ViewBinding implements Unbinder {
   private LeisureFragment target;
 
-  private View view2131624262;
+  private View view2131624264;
 
   @UiThread
   public LeisureFragment_ViewBinding(final LeisureFragment target, View source) {
@@ -25,7 +25,7 @@ public class LeisureFragment_ViewBinding implements Unbinder {
     target.yueduLayout = Utils.findRequiredViewAsType(source, R.id.yuedu_layout, "field 'yueduLayout'", FrameLayout.class);
     view = Utils.findRequiredView(source, R.id.twists_layout, "field 'twistsLayout' and method 'onClick'");
     target.twistsLayout = Utils.castView(view, R.id.twists_layout, "field 'twistsLayout'", FrameLayout.class);
-    view2131624262 = view;
+    view2131624264 = view;
     view.setOnClickListener(new DebouncingOnClickListener() {
       @Override
       public void doClick(View p0) {
@@ -62,7 +62,7 @@ public class LeisureFragment_ViewBinding implements Unbinder {
     target.sougou_layout = null;
     target.shenhuifuLayout = null;
 
-    view2131624262.setOnClickListener(null);
-    view2131624262 = null;
+    view2131624264.setOnClickListener(null);
+    view2131624264 = null;
   }
 }
